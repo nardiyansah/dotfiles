20d50d9e2df1e7b0ed10e3e392949ec19da4b258
nardiyansah <nardiyansah.jatmiko@bytedance.com>
2025-08-05 14:35:48 +0700

    rewrite nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,76 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- README
+-- Structure File:
+-- 1. Bootstrap lazy.nvim
+-- 2. Plugins
+-- 3. Keymaps
+-- 4. Autocommands
+-- 5. Options
+--
+-- Each section begin with:
+-- ### SECTION_NAME ###
+
+-- ### BOOTSTRAP lazy.nvim ###
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- ### PLUGINS ###
+require("lazy").setup({
+  spec = {
+    -- add your plugins here
+    {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+      },
+    },
+    {
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+	}
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+
+-- ### KEYMAPS ###
+
+-- ### AUTOCOMMANDS ###
+
+-- ### OPTIONS ###
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.scrolloff = 8
