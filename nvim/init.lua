c2599b599efb580af9513b984042ad4d45b505d0
nardiyansah <nardiyansah.jatmiko@bytedance.com>
2025-09-02 06:35:01 +0700

    set autoimport go

--- nvim/init.lua
+++ nvim/init.lua
@@ -49,7 +49,9 @@ require("lazy").setup({
     },
     {
 		"mason-org/mason-lspconfig.nvim",
-		opts = {},
+		opts = {
+			ensure_installed = { 'gopls', 'lua-language-server' },
+		},
 		dependencies = {
 			{ "mason-org/mason.nvim", opts = {} },
 			"neovim/nvim-lspconfig",
@@ -124,6 +126,16 @@ require 'nvim-treesitter.configs'.setup {
 	ensure_installed = { 'lua', 'go' },
 	highlight = { enable = true }
 }
+
+-- go setting
+local lspconfig = require('lspconfig')
+lspconfig.gopls.setup({
+	settings = {
+		gopls = {
+			gofumpt = true
+		}
+	}
+})
 
 -- ### KEYMAPS ###
 -- trigger omni autocomplete
@@ -217,6 +229,30 @@ vim.api.nvim_create_autocmd({ "VimEnter", "BufEnter" }, {
 	  Service.set_active_list(bookmark_list.id)
 	  require("bookmarks.sign").safe_refresh_signs()
 	end
+})
+
+-- set auto import and formatting from go official: https://go.dev/gopls/editor/vim#neovim-imports
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = {only = {"source.organizeImports"}}
+    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+    -- machine and codebase, you may want longer. Add an additional
+    -- argument after params if you find that you have to write the file
+    -- twice for changes to be saved.
+    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+    vim.lsp.buf.format({async = false})
+  end
 })
 
 -- ### OPTIONS ###
